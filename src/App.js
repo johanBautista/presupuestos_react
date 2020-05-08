f0b6a47f537f2003fc035ce442c06e9ffb4cfd1c
johan Bautista <jsbparra@gmail.com>
2020-05-08 08:03:57 +0200

    comp-Pregunta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Pregunta from './components/Pregunta';
 
 function App() {
   return (
-    <div className="App">
-      <h2>hi!!</h2>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+      </header>
     </div>
   );
 }
