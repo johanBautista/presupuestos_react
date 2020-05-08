dedc179847e172b635979c3434cb5153c86714a3
johan Bautista <jsbparra@gmail.com>
2020-05-08 12:27:22 +0200

    add helper revisarpresupuesto

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
+import { revisarPresupuesto } from '../helpers';
 
 const ControlPresupuesto = ({ presupuesto, restante }) => {
   return (
     <Fragment>
-      <div className="alert alert-primary">
-        Presupuesto: {presupuesto}
-      </div>
-      <div className="alert">
+      <div className="alert alert-primary">Presupuesto: {presupuesto}</div>
+      <div className={revisarPresupuesto(presupuesto, restante)}>
         Restante: {restante}
       </div>
     </Fragment>
