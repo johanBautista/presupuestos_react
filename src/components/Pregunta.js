6ab1d98907239c9d7e52f9658b17c24a2f1c2071
johan Bautista <jsbparra@gmail.com>
2020-05-08 08:34:50 +0200

    state-cantidad, state-error, def-presupuesto, agr-presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,14 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+  // state cantidad ingresada
+  const [cantidad, setcantidad] = useState(0);
+  // state error
+  const [error, seterror] = useState(false);
+
+  // captura el presupuesto ingresado
+  const definirPresupuesto = (e) => {
+    setcantidad(parseInt(e.target.value));
+  };
+
+  // agregar presupuesto
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+    //validacion
+    if (cantidad <= 0 || isNaN(cantidad)) {
+      seterror(true);
+      return;
+    }
+    //eliminar error
+    seterror(false);
+  };
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
-      <form action="">
+      {error ? 'ingresa un presupuesto correcto' : null}
+      <form onSubmit={agregarPresupuesto}>
         <input
           type="number"
           className="u-full-width"
           placeholder="ingresa tu presupuesto"
+          onChange={definirPresupuesto}
         />
         <input
           type="submit"
